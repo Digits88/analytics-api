69368f2233f6f1193dd560b80e465b552f54ee24
Anssi Piirainen <api@iki.fi>
2017-12-12 11:32:52 +0200

    Fix the input validation to `GET /videos/:id`

--- __tests__/test_api.js
+++ __tests__/test_api.js
@@ -34,7 +34,7 @@ function request(method, url, data, query) {
 
 
 it('should get video stats', async () => {
-    const resp = await request("get", "/videos/360b8f49-3c98-4020-ac72-83f958405239", {
+    const resp = await request("get", "/videos/360b8f49-3c98-4020-ac72-83f958405239", null, {
         starts: '2017-12-01T00',
         ends: '2017-12-10T00'
     });
